4f0d57f1d5abf141a735bbf19e64fd16c8854b56
ltcSpitfire <ltc.spitfire@gmail.com>
2018-04-04 23:46:38 +0300

    Sheep movement

---
+++ HerdTheHorde/Assets/SheepMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepMovement : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+    public bool isMoving;
+    public float moveTime;
+    private float moveCounter;
+    public float waitTime;
+    private float waitCounter;
+    private int moveDirection;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        waitCounter = waitTime;
+        moveCounter = moveTime;
+
+        ChooseDirection();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (isMoving)
+        {
+            moveCounter -= Time.deltaTime;
+
+            switch (moveDirection)
+            {
+            case 0:
+                    myRigidbody.velocity = new Vector2(0, moveSpeed);
+                    break;
+
+            case 1:
+                    myRigidbody.velocity = new Vector2(moveSpeed, 0);
+                    break;
+
+            case 2:
+                    myRigidbody.velocity = new Vector2(0, -moveSpeed);
+                    break;
+
+            case 3:
+                    myRigidbody.velocity = new Vector2(-moveSpeed, 0);
+                    break;
+            }
+
+            if (moveCounter < 0)
+            {
+                isMoving = false;
+                waitCounter = waitTime;
+            }
+        }
+        else
+        {
+            waitCounter -= Time.deltaTime;
+
+            myRigidbody.velocity = Vector2.zero;
+
+            if(waitCounter < 0)
+            {
+                ChooseDirection();
+            }
+        }
+	}
+
+    public void ChooseDirection()
+    {
+        moveDirection = Random.Range(0, 4);
+        isMoving = true;
+        moveCounter = moveTime;
+    }
+}
