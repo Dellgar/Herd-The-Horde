ba0bc6ab26e262fcc1a633f18b179cf77d248c00
NK <m8koni@gmail.com>
2018-04-04 12:20:48 +0300

    Basic Dragging functionality

--- HerdTheHorde/Assets/Scripts/Draggable.cs
+++ HerdTheHorde/Assets/Scripts/Draggable.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Draggable : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	void OnMouseDown()
+	{
+
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnMouseDrag()
+	{
+		Vector2 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+		Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
 	}
+
 }
