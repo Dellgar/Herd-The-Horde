8c0b987914a3dd539baa54394786ad4f76dbb13d
NK <m8koni@gmail.com>
2018-04-05 14:27:36 +0300

    Cursor

---
+++ HerdTheHorde/Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour {
+
+   
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(cursorPosition.x, cursorPosition.y-2.3f);
+
+
+	}
+
+
+}
