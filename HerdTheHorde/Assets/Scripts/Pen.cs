7900de66a1285cb7cb66ea257398b4919d1d1bcf
NK <m8koni@gmail.com>
2018-04-06 15:30:46 +0300

    Pencounter that doesnt work YET

---
+++ HerdTheHorde/Assets/Scripts/Pen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Pen : MonoBehaviour {
+
+    List<Collider> collidedObj = new List<Collider>();
+    public int sheepCount;
+    public TextMesh penText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        penText.text = sheepCount.ToString();
+		
+	}
+
+    void OnTriggerStay2D(Collider2D obj)
+    {
+        collidedObj.Add(obj.GetComponent<Collider>());
+        Debug.Log("collided lol");
+    }
+}
