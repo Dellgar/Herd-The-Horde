1ddd7be167be441ff7960b54b6e4a5f080249e0a
NK <m8koni@gmail.com>
2018-04-09 13:17:20 +0300

    Fixing sheep freezing, adding sheepcounter for pens

---
+++ HerdTheHorde/Assets/Scripts/PenChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenChecker : MonoBehaviour {
+
+    private TextMesh penText;
+    private int sheepsInsidePen;
+
+	// Use this for initialization
+	void Awake () {
+        penText = gameObject.GetComponentInChildren<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void UpdateScore () {
+        penText.fontSize = 15;
+        penText.text = "Sheeps: " + sheepsInsidePen;
+	}
+
+    public void CorrectPenForSheep(int sheepCount)
+    {
+        sheepsInsidePen += sheepCount;
+        UpdateScore();
+        return;
+    }
+}
--- HerdTheHorde/Assets/Scripts/SheepCollision.cs
+++ HerdTheHorde/Assets/Scripts/SheepCollision.cs
@@ -6,44 +6,40 @@ using UnityEngine;
 
 public class SheepCollision : MonoBehaviour {
 
-    public int sheepCount;
+    public int sheepPointValue = 1;
 
 	//private TextMesh sheepPenText;
 	[SerializeField]
-	private bool isColliding;
+	private bool isCollidingEnter;
 
-	private SheepMovement movement_OtherScript;
+    private SheepMovement movement_OtherScript;
 
-    void Awake() {
+    void Awake()
+    {
 		movement_OtherScript = GetComponent<SheepMovement>();
 	}
 
-	void Start() {
+    void OnTriggerEnter2D(Collider2D colGameObject)
+    {
+		if (isCollidingEnter) return;
 
-	}
-
-	void Update() {
-
-	}
-
-    void OnTriggerEnter2D(Collider2D colGameObject) {
-		if (isColliding) return;
-
-		isColliding = true;
-		movement_OtherScript.enabled = false;
-		if (colGameObject.CompareTag(gameObject.tag))
-		{
-			//sheepCount++;
-			Debug.Log("Right Pen");
-			gameObject.GetComponent<Collider2D>().isTrigger = true;
-		}
-		else
+		isCollidingEnter = true;
+        if (colGameObject.CompareTag(gameObject.tag))
+        {
+            Debug.Log("Right Pen");
+            colGameObject.gameObject.GetComponent<PenChecker>().CorrectPenForSheep(sheepPointValue);
+            gameObject.GetComponent<Collider2D>().isTrigger = true;
+            movement_OtherScript.enabled = false;
+        }
+        else
 		{
 			Debug.Log("Wrong Pen");
 		}
 	}
-	void OnTriggerExit2D(Collider2D colGameObject) {
-		//movement_OtherScript.enabled = true;
+	void OnTriggerExit2D(Collider2D colGameObject)
+    {    
+        //gameObject.GetComponent<Collider2D>().isTrigger = false;
+        //movement_OtherScript.enabled = true;
 	}
 
 }
