90a288f85e3b9ee27e2e461c9183f56751540379
ltcSpitfire <ltc.spitfire@gmail.com>
2018-04-10 13:39:34 +0300

    Game Manager + level complete + max sheep in pen

---
+++ HerdTheHorde/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public int penTotal;
+    public int finishedPens;
+
+    void Update()
+    {
+        if (penTotal == finishedPens)
+        {
+            SceneManager.LoadScene(0, LoadSceneMode.Single);
+        }
+    }
+
+    public void LevelComplete(int fPens)
+    {
+        finishedPens += fPens;
+    }
+}
--- HerdTheHorde/Assets/Scripts/PenChecker.cs
+++ HerdTheHorde/Assets/Scripts/PenChecker.cs
@@ -6,26 +6,44 @@ public class PenChecker : MonoBehaviour {
 
     private TextMesh penText;
     private int sheepsInsidePen;
+    public int sheepGoal;
+    [SerializeField] private bool isSheepGoalAchieved;
+    public GameManager gManagerScript;
 
 	void Awake ()
     {
         penText = gameObject.GetComponentInChildren<TextMesh>();
+        gManagerScript = GameObject.Find("_manager").GetComponent<GameManager>();
 	}
 
     private void Update()
     {
         UpdateScore();
+        PenCompleted();
     }
 
     void UpdateScore ()
     {
         penText.fontSize = 15;
-        penText.text = " " + sheepsInsidePen;
+        penText.text = sheepsInsidePen + " / " + sheepGoal;
 	}
 
     public void CorrectPenForSheep(int sheepCount)
     {
         sheepsInsidePen += sheepCount;
         UpdateScore();
+    }
+
+    void PenCompleted()
+    {
+        if (isSheepGoalAchieved)
+            return;
+
+        if (sheepsInsidePen >= sheepGoal)
+        {
+            Debug.Log("Pen finished");
+            isSheepGoalAchieved = true;
+            gManagerScript.LevelComplete(1);
+        }
     }
 }
