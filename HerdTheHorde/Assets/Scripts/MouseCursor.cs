0468c26bde02a2f08a13497ff4c1f3c3a5e50c4e
NK <m8koni@gmail.com>
2018-04-24 15:15:10 +0300

    initial_black + logo with wolf

--- HerdTheHorde/Assets/Scripts/MouseCursor.cs
+++ HerdTheHorde/Assets/Scripts/MouseCursor.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class MouseCursor : MonoBehaviour {
 
-   
-
-	// Use this for initialization
-	void Start () {
-        //Cursor.visible = false;
+    public Sprite cursorNormalSprite;
+    public Sprite cursorPressSprite;
+    public SpriteRenderer myRenderer;
+    
+    void Start ()
+    {
+        if (myRenderer == null) myRenderer.GetComponent<SpriteRenderer>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void Update () {
+
         Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector2(cursorPosition.x+0.4f, cursorPosition.y-1.4f);
-	}
 
-
+        if (Input.GetKey(KeyCode.Mouse0)) myRenderer.sprite = cursorPressSprite;
+        else myRenderer.sprite = cursorNormalSprite;
+    }
 }
