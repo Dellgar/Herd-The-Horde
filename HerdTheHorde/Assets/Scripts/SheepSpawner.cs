485cab3d801c94939cf4e2663234a48a1231edc5
NK <m8koni@gmail.com>
2018-05-21 11:36:08 +0300

    Spawner simplified
    for easier testing

--- HerdTheHorde/Assets/Scripts/SheepSpawner.cs
+++ HerdTheHorde/Assets/Scripts/SheepSpawner.cs
@@ -6,7 +6,7 @@ public class SheepSpawner : MonoBehaviour {
 
     private GameManager gmScript;
 
-    public string[] spawnableRaces;         //List of spawnable sheep races
+    //public string[] spawnableRaces;         //List of spawnable sheep races
     private int listMin, listMax;           //Set the prefab restriction what to spawn, ie. white 0 - 2; inclusive - exclusive
     public GameObject[] spawnPrefabs;		//List of prefabs that can be spawned
     public Vector3[] spawnPoint;           //List of the spawnpoints
@@ -23,14 +23,15 @@ public class SheepSpawner : MonoBehaviour {
         spawnerReady = true;
         gmScript.spawnerActive = true;
 
-        StartCoroutine(Spawning());
+       StartCoroutine(SpawnPrefabSheep());
+        //StartCoroutine(Spawning());
     }
 
 
 	
 	void Update ()
     {
-        if(spawningInterval <= 1f) spawningInterval = 1.8f;
+        //if(spawningInterval <= 1f) spawningInterval = 1.8f;
 
         for (int i = 0; i < 9; ++i)
         {
@@ -54,50 +55,71 @@ public class SheepSpawner : MonoBehaviour {
 		}
 	}
 
-	IEnumerator Spawning()
+    IEnumerator SpawnPrefabSheep()
     {
         while (gmScript.spawnerActive)
         {
             yield return new WaitForSeconds(spawningInterval);
+
             //choose a spawning point
             spawnPointIndex = Random.Range(0, spawnPoint.Length);
 
-            //choose a race at random
-            int rndRace = Random.Range(0, spawnableRaces.Length);
-            
-            switch (spawnableRaces[rndRace])
-            {
-                case "white":
-                    listMin = 0;
-                    listMax = 1;
-                    //Debug.Log("race: white");
-                    break;
+            GameObject sheep = Instantiate(spawnPrefabs[Random.Range(0, spawnPrefabs.Length)],
+                new Vector2(spawnPoint[spawnPointIndex].x, spawnPoint[spawnPointIndex].y), Quaternion.identity);
 
-                case "black":
-                    listMin = 5;
-                    listMax = 6;
-                    //Debug.Log("race: black");
-                    break;
-
-                default:
-                    Debug.Log("not of any race");
-                    break;
-            }
-
-            //limit prefab pool to spawn only from one race
-            int sheepPrefab = Random.Range(listMin, listMax);
-
-            gmScript.SheepSpawn(1, spawnableRaces[rndRace]);
-
-            GameObject sheep = Instantiate(spawnPrefabs[sheepPrefab],
-                new Vector2(spawnPoint[spawnPointIndex].x,
-                spawnPoint[spawnPointIndex].y), Quaternion.identity);
             sheep.name = "Sheep #" + gmScript.sheepSpawned.ToString();
 
             Debug.Log("Sheep Spawned");
+            gmScript.SheepSpawn(1, sheep.GetComponent<Sheep>().race);
 
-            spawningInterval = spawningInterval - 0.08f;
+            //spawningInterval = spawningInterval - 0.08f;
 
         }
     }
+    //IEnumerator Spawning()
+    //{
+    //    while (gmScript.spawnerActive)
+    //    {
+    //        yield return new WaitForSeconds(spawningInterval);
+    //        //choose a spawning point
+    //        spawnPointIndex = Random.Range(0, spawnPoint.Length);
+
+    //        //choose a race at random
+    //        int rndRace = Random.Range(0, spawnableRaces.Length);
+            
+    //        switch (spawnableRaces[rndRace])
+    //        {
+    //            case "white":
+    //                listMin = 0;
+    //                listMax = 1;
+    //                //Debug.Log("race: white");
+    //                break;
+
+    //            case "black":
+    //                listMin = 5;
+    //                listMax = 6;
+    //                //Debug.Log("race: black");
+    //                break;
+
+    //            default:
+    //                Debug.Log("not of any race");
+    //                break;
+    //        }
+
+    //        //limit prefab pool to spawn only from one race?
+    //        int sheepPrefab = Random.Range(listMin, listMax);
+
+    //        gmScript.SheepSpawn(1, spawnableRaces[rndRace]);
+
+    //        GameObject sheep = Instantiate(spawnPrefabs[sheepPrefab],
+    //            new Vector2(spawnPoint[spawnPointIndex].x,
+    //            spawnPoint[spawnPointIndex].y), Quaternion.identity);
+    //        sheep.name = "Sheep #" + gmScript.sheepSpawned.ToString();
+
+    //        Debug.Log("Sheep Spawned");
+
+    //        spawningInterval = spawningInterval - 0.08f;
+
+    //    }
+    //}
 }
