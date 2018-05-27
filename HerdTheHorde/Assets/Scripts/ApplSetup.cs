a0bf37a6ec8c819a32a830f2fc06401b0764881d
m8koni <m8koni@gmail.com>
2018-05-28 02:53:49 +0300

    Mobileport setup

---
+++ HerdTheHorde/Assets/Scripts/ApplSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplSetup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		Application.targetFrameRate = 35;
+
+		QualitySettings.vSyncCount = 0;
+
+		QualitySettings.antiAliasing = 0;
+
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+	}
+}
